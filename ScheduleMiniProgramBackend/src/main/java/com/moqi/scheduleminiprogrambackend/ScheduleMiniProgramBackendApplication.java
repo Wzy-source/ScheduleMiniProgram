7bc95c1378fd49b37e8abb11c203c57f69627631
moqi <1402290478@qq.com>
2021-08-16 19:11:39 +0800

    后端初始化

---
+++ ScheduleMiniProgramBackend/src/main/java/com/moqi/scheduleminiprogrambackend/ScheduleMiniProgramBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moqi.scheduleminiprogrambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleMiniProgramBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleMiniProgramBackendApplication.class, args);
+    }
+
+}
---
+++ ScheduleMiniProgramBackend/src/test/java/com/moqi/scheduleminiprogrambackend/ScheduleMiniProgramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moqi.scheduleminiprogrambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleMiniProgramBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
